2616393e3faa6bcf5d57cb216d122c118b959543
woong <wjdwldnd254@naver.com>
2025-08-29 16:20:39 +0900

    0829

--- 04_mysql/app.js
+++ 04_mysql/app.js
@@ -1,38 +1,81 @@
 // app.js
 const express = require("express");
+const parser = require("body-parser");
+const sql = require("./sql");
+const prodSql = require("./sql/sql");
+const cors = require("cors");
+// console.log(prodSql["productMainImage"].query);
 
-const parser = require("body-parser"); // body정보 해석처리.
-const sql = require("./sql"); // sql/index.js
-
-const app = express(); // express 인스턴스.
+const app = express();
 app.use(parser.urlencoded()); // x-www-form-urlencoded
-app.use(parser.json()); // application/json
+app.use(parser.json());
+app.use(cors());
 
 app.get("/", (req, resp) => {
   resp.send("/ 실행");
 });
 
+// 상품쿼리.
+app.post("/api/:alias", async (req, resp) => {
+  let search = prodSql[req.params.alias].query; // alias: productDetail
+  let param = req.body.param; // param: [2]
+  try {
+    let result = await sql.excute(search, param);
+    resp.json(result);
+  } catch (err) {
+    console.error("SQL 실행 오류:", err);
+    resp.json({ retCode: "Error" });
+  }
+});
+
 // 고객목록.
 app.get("/customers", async (req, resp) => {
   try {
-    let customerList = await sql.excute("select * from customers");
-    console.log(customerList);
+    let customerList = await sql.execute("select * from customers");
     resp.json(customerList);
   } catch (err) {
-    console.log(err);
     resp.json({ retCode: "Error" });
   }
 });
-
+app.get("/products", async (req, resp) => {
+  try {
+    resp.json([
+      {
+        product_name: "기계식 키보드",
+        price: 25000,
+        category: "노트북/태블릿",
+        delivery_price: 5000,
+      },
+      {
+        product_name: "무선 마우스",
+        price: 12000,
+        category: "노트북/태블릿",
+        delivery_price: 5000,
+      },
+      {
+        product_name: "아이패드",
+        price: 725000,
+        category: "노트북/태블릿",
+        delivery_price: 5000,
+      },
+      {
+        product_name: "무선충전기",
+        price: 42000,
+        category: "노트북/태블릿",
+        delivery_price: 5000,
+      },
+    ]);
+  } catch (err) {
+    resp.json({ retCode: "Error" });
+  }
+});
 // 등록.
 app.post("/customer", async (req, resp) => {
-  console.log(req.body.param);
   try {
-    let result = await sql.excute(
+    let result = await sql.execute(
       "insert into customers set ?", //
       [req.body.param]
     );
-    console.log(result);
     resp.json(result);
   } catch (err) {
     console.log(err);
@@ -40,30 +83,28 @@ app.post("/customer", async (req, resp) => {
   }
 });
 
-// 삭제
+// http://localhost:8080/boardList.do?page=3
+// http://localhost:3000/customer/:id
 app.delete("/customer/:id", async (req, resp) => {
-  console.log(req.params.id);
   try {
-    let result = await sql.excute(
+    let result = await sql.execute(
       "delete from customers where id = ?", //
-      parseInt(req.params.id)
+      [req.params.id]
     );
-    console.log(result);
     resp.json(result);
   } catch (err) {
-    console.log(err);
     resp.json({ retCode: "Error" });
   }
 });
-// 수정
-app.put("/customer/:id", async (req, resp) => {
-  console.log(req.body.param);
+
+//수정.
+app.put("/customer", async (req, resp) => {
+  console.log(req.body);
   try {
-    let result = await sql.excute(
-      "update customers set ? where id = ?", //
-      [req.body.param, parseInt(req.params.id)]
+    let result = await sql.execute(
+      "update customers set name=?,email=?,phone=? where id = ?", //
+      [req.body.name, req.body.email, req.body.phone, req.body.id]
     );
-    console.log(result);
     resp.json(result);
   } catch (err) {
     console.log(err);
